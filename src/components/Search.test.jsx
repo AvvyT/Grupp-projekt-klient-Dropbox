a8c8776879adcabede58489cf8781fd638c56b4a
Radoslava Tsankova Todorova <radoslavatsankovatodorova@MacBook-de-Radoslava.local>
2019-05-14 10:08:35 +0200

    last finish to test-Avvy

--- src/components/Search.test.jsx
+++ src/components/Search.test.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Search from './Search.jsx';
-import chai, { expect } from 'chai';  
+import chai, { expect } from 'chai';
 import chaiJestSnapshot from "chai-jest-snapshot";
+import { shallow, configure } from 'enzyme';
 
-chai.use(chaiJestSnapshot);
+import Adapter from "enzyme-adapter-react-16"
+configure({ adapter: new Adapter() });
 
 /** ---I combine two testing approaches: Snapshot Testing and Component logic testing---**/
 /**
  * Snapshot Testing is a useful testing tool in case you want to be sure user interface hasn’t changed.
  **/
-import { shallow, configure, mount } from 'enzyme';
 
-import Adapter from "enzyme-adapter-react-16"
-configure({ adapter: new Adapter() });
-
+chai.use(chaiJestSnapshot); // with mocha, it is recommended to use chai-jest-snapshot 
 // skapa en mock fun.
 const mockPrevent = jest.fn();
 
 // Testing one isolated function, or one React component. Enzyme’s shallow() is a unit test.
 describe('Search', () => {
-    it(' render correctly ', () => {
+    it('render correctly ', () => {
         const funComponent = shallow(<Search />);
 
         expect(funComponent).to.matchSnapshot();
@@ -51,6 +50,20 @@ describe('Search', () => {
         expect(wrapper.find('input[type="text"]').prop("value")).to.equal('');
         wrapper.find('input[type="text"]').simulate('change', { target: { value: 'Changed' } });
         expect(wrapper.find('input[type="text"]').prop("value")).to.equal('Changed');
+    });
+
+    it('after submitted,the input should be empty', () => {
+        const wrapper = shallow(<Search />);
+
+        expect(wrapper.find('input[type="text"]').prop("value")).to.equal('');
+        wrapper.find('input[type="text"]').simulate('change', { target: { value: 'changet to Eva' } });
+        expect(wrapper.find('input[type="text"]').prop("value")).to.equal('changet to Eva');
+        console.log(wrapper.find('input[type="text"]').prop("value"));
+        
+        wrapper.find('form').simulate('submit', {
+            preventDefault: mockPrevent
+        })
+        expect(wrapper.find('input[type="text"]').prop("value")).to.equal('');
     });
 
 });
