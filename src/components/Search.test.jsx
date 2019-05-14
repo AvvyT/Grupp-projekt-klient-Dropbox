666575a8f69efd654a61d59430279d5681b84003
Radoslava Tsankova Todorova <radoslavatsankovatodorova@MacBook-de-Radoslava.local>
2019-05-14 08:44:10 +0200

    more test

--- src/components/Search.test.jsx
+++ src/components/Search.test.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Search from './Search.jsx';
+import chai, { expect } from 'chai';  
+import chaiJestSnapshot from "chai-jest-snapshot";
+
+chai.use(chaiJestSnapshot);
 
 /** ---I combine two testing approaches: Snapshot Testing and Component logic testing---**/
 /**
@@ -16,10 +20,10 @@ const mockPrevent = jest.fn();
 
 // Testing one isolated function, or one React component. Enzyme’s shallow() is a unit test.
 describe('Search', () => {
-    it('should render correctly in "debug" mode', () => {
-        const funComponent = shallow(<Search debug />);
+    it(' render correctly ', () => {
+        const funComponent = shallow(<Search />);
 
-        expect(funComponent).toMatchSnapshot();
+        expect(funComponent).to.matchSnapshot();
         // toMatchSnapshot() => method creates Snapshot itself
     });
 
@@ -34,33 +38,19 @@ describe('Search', () => {
     it('form gets submitted once', () => {
         const wrapper = shallow(<Search />);
 
-        expect(wrapper.find('input').length).toBe(2)
+        expect(wrapper.find('input').length).to.equal(2)
         // preventDefault-metod: läggs mock-fun
         wrapper.find('form').simulate("submit", {
             preventDefault: mockPrevent
         })
-        expect(mockPrevent.mock.calls.length).toBe(1)
+        expect(mockPrevent.mock.calls.length).to.equal(1)
     });
 
-
-
-    /**---Snapshot Testing---**/
-    // Testing a multitude of functions working together, or an entire React component including children components. 
-    // Enzyme’s mount() is an integration test.
-    it('should be possible to activate form-submit with Spacebar', () => {
-        const funComponent = mount(<Search />);
-        funComponent
-            .find('form')
-            .simulate('keydown', { keyCode: 32 });
-        expect(funComponent).toMatchSnapshot();
-        funComponent.unmount();
-    });
-
-
-    it('should render banner text correctly with given strings', () => {
-        const strings = ['one', 'two'];
-        const funComponent = shallow(<Search list={strings} />);
-        expect(funComponent).toMatchSnapshot();
+    it('change works', () => {
+        const wrapper = shallow(<Search />);
+        expect(wrapper.find('input[type="text"]').prop("value")).to.equal('');
+        wrapper.find('input[type="text"]').simulate('change', { target: { value: 'Changed' } });
+        expect(wrapper.find('input[type="text"]').prop("value")).to.equal('Changed');
     });
 
 });
