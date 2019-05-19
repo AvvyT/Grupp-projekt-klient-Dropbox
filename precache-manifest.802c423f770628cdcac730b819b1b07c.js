1b9b14948ab288a61d5fef06691fb91be9722aad
Radoslava Tsankova Todorova <radoslavatsankovatodorova@MacBook-de-Radoslava.local>
2019-05-19 22:17:24 +0200

    Updates

---
+++ precache-manifest.802c423f770628cdcac730b819b1b07c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7def7061342562dd7852f72338a6398",
+    "url": "/Grupp-projekt-klient-Dropbox/index.html"
+  },
+  {
+    "revision": "3cfab96be223c2b7c400",
+    "url": "/Grupp-projekt-klient-Dropbox/static/css/main.48eda2be.chunk.css"
+  },
+  {
+    "revision": "65083d46013d2397e4fe",
+    "url": "/Grupp-projekt-klient-Dropbox/static/js/2.0aa216e0.chunk.js"
+  },
+  {
+    "revision": "3cfab96be223c2b7c400",
+    "url": "/Grupp-projekt-klient-Dropbox/static/js/main.76853c14.chunk.js"
+  },
+  {
+    "revision": "d7f4972a56ce530b73b5",
+    "url": "/Grupp-projekt-klient-Dropbox/static/js/runtime~main.d878ce96.js"
+  }
+]);
